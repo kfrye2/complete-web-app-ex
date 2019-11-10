85e4210e34eb79d98a1fecee83ce71f90836f3b3
Kristin Frye <kfrye2@mail.umw.edu>
2019-11-10 13:51:32 -0500

    Added frontend and backend

---
+++ frontend/.next/static/webpack/static/development/pages/search.js.8f6b61e7b924f7b243eb.hot-update.js
@@ -0,0 +1,47 @@
+webpackHotUpdate("static/development/pages/search.js",{
+
+/***/ "./lib/utils.js":
+/*!**********************!*\
+  !*** ./lib/utils.js ***!
+  \**********************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _interopRequireDefault = __webpack_require__(/*! @babel/runtime-corejs2/helpers/interopRequireDefault */ "./node_modules/@babel/runtime-corejs2/helpers/interopRequireDefault.js");
+
+var _bluebird = _interopRequireDefault(__webpack_require__(/*! bluebird */ "./node_modules/bluebird/js/browser/bluebird.js"));
+
+__webpack_require__(/*! isomorphic-fetch */ "./node_modules/isomorphic-fetch/fetch-npm-browserify.js");
+
+function getJson(url) {
+  return fetch(url).then(function (resp) {
+    console.log(url, resp);
+    return resp.json();
+  });
+}
+
+function handleError(error) {
+  console.warn(error);
+  return null;
+}
+
+function getCampground(name) {
+  //return fetch(`http://35.196.38.200/api/info?q=${name}`).then(function(resp) {
+  return fetch("http://localhost:8080/api/info?q=".concat(name)).then(function (resp) {
+    return resp.json();
+  });
+}
+
+module.exports = {
+  getInfo: function getInfo(player) {
+    return getCampground(player)["catch"](handleError);
+  }
+};
+
+/***/ })
+
+})
+//# sourceMappingURL=search.js.8f6b61e7b924f7b243eb.hot-update.js.map
---
+++ frontend/lib/utils.js
@@ -0,0 +1,27 @@
+require("isomorphic-fetch");
+import BPromise from "bluebird";
+
+function getJson(url) {
+  return fetch(url).then(function(resp) {
+    console.log(url, resp);
+    return resp.json();
+  });
+}
+
+function handleError(error) {
+  console.warn(error);
+  return null;
+}
+
+function getCampground(name) {
+  //return fetch(`http://35.196.38.200/api/info?q=${name}`).then(function(resp) {
+    return fetch(`http://localhost:8080/api/info?q=${name}`).then(function(resp) {
+    return resp.json();
+  });
+}
+
+module.exports = {
+  getInfo: function(player) {
+    return getCampground(player).catch(handleError);
+  }
+};
